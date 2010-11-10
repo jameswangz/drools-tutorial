c277fc1a40f69d7c826bfb1db9e784b2abac9c02
James Wang <java2enterprise@gmail.com>
2010-11-10 17:32:56 +0800

    Refactored tests

---
+++ src/main/java/com/snda/infrastrure/drools/tutorial/Customer.java
@@ -0,0 +1,25 @@
+package com.snda.infrastrure.drools.tutorial;
+
+public class Customer {
+	
+	private final String firstName;
+	private boolean greeted;
+
+	public Customer(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public boolean isGreeted() {
+		return greeted;
+	}
+
+	public void setGreeted(boolean greeted) {
+		this.greeted = greeted;
+	}
+
+	
+}
---
+++ src/test/java/hamcrest/Ensure.java
@@ -0,0 +1,33 @@
+package hamcrest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.hamcrest.Matcher;
+import org.hamcrest.core.IsEqual;
+import org.hamcrest.core.IsNot;
+import org.hamcrest.core.IsNull;
+import org.hamcrest.core.IsSame;
+
+public abstract class Ensure {
+
+	public static void ensureThat(boolean matches) {
+		ensureThat(matches, new IsSame<Boolean>(true));
+	}
+
+	public static <T> void ensureThat(T actual, Matcher<T> matcher) {
+		assertThat(actual, matcher);
+	}
+
+	public static boolean not(boolean matches) {
+		return !matches;
+	}
+
+	public static <T> Matcher<T> shouldBe(T expected) {
+		return new IsEqual<T>(expected);
+	}
+
+	public static <T> Matcher<T> isNotNull() {
+		return new IsNot<T>(new IsNull<T>());
+	}
+
+}
